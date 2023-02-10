8a4ca59529a35055ccd78149020dd3be34db3b71
angelhdzmultimedia <angel.hernandez.12@live.com>
2023-02-09 23:12:54 -0400

    4 files modified, 1 added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
+import { collectTaskItems, SelectStageVO } from './app'
+import { join } from 'node:path'
+declare global {
+  interface JSON {
+    load: (url: string) => Promise<any>
+  }
+}
 
+JSON.load = async function (url: string) {
+  const { readFileSync } = await import('node:fs')
+  const file: string = readFileSync(url).toString()
+  return JSON.parse(file)
+}
+
+async function main() {
+  const data = await JSON.load(join(process.cwd(), 'src/data.json'))
+  const stageVO = new SelectStageVO()
+  await collectTaskItems(stageVO, data.items, {}, 0)
+  console.log(stageVO.items)
+}
+
+main()
